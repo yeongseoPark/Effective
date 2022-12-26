be45ae7907a66b1daf7ee42cc0654a92160ef2fa
YSEO <1park4170@gmail.com>
2022-12-26 20:35:44 +0900

    ch7

---
+++ src/main/java/Chapter2/Item7/Stack.java
@@ -0,0 +1,32 @@
+package Chapter2.Item7;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+    private Object[] elements;// 객체 참조를 담는 elements 배열, 자기 메모리를 직접 관리
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+        Object result = elements[--size]; // 비활성 영역(줄어든 영역)은 쓰이지 않지만 null처리 하지 않으면 GC가 알 방법이 없다
+        elements[size] = null; // 따라서 다쓴 참조를 null처리해서 해제하지 않으면 GC가 Obsolete reference를 계속 가지고 있는다
+        return result;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size)
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+    }
+}
---
+++ src/main/java/Chapter2/Item7/stock.java
@@ -0,0 +1,30 @@
+package Chapter2.Item7;
+
+import java.io.Console;
+import java.lang.ref.WeakReference;
+
+public class stock {
+    String name;
+
+    public stock(String name) {
+        this.name = name;
+    }
+}
+
+class gc {
+    public static void main(String[] args) {
+        stock phone = new stock("phone");
+
+        WeakReference<stock> weak  = new WeakReference<>(phone);
+
+        System.out.println(weak.get().name);
+
+        // 할당 해지
+        phone = null;
+
+        System.gc();
+
+        System.out.println(weak); // WeakReference 자체를 할당해제하는 것은 아니고, 안에 담긴 reference를 gc가 해제.
+        System.out.println(weak.get().name); // 따라서 .get() 한 결과물이 null이 되어 npe 발생
+    }
+}
